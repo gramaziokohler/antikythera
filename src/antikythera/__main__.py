252c85bd5d41264a1f03f8f0bd313b40fc8463f1
Chen Kasirer <ckasirer@ethz.ch>
2025-12-02 10:09:19 +0100

    removed the CORS middleware

--- src/antikythera/__main__.py
+++ src/antikythera/__main__.py
@@ -83,18 +83,6 @@ class UploadBlueprintResponse(BaseModel):
 
 
 app = FastAPI(title="Antikythera Orchestrator API")
-
-# TODO: not to do. do redirection like Gonzalo said.
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",  # Vite default dev server
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allows all headers
-)
-
 _sessions_lock = Lock()
 _sessions: Dict[str, ActiveSession] = {}
 
