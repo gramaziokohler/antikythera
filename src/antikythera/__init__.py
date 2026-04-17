0bcc8f78635216e4e47795b2065c209917bae819
Chen Kasirer <ckasirer@ethz.ch>
2026-04-17 16:41:05 +0200

    Bump version to 0.1.1

--- src/antikythera/__init__.py
+++ src/antikythera/__init__.py
@@ -2,6 +2,6 @@ __author__ = ["Gonzalo Casas", "Chen Kasirer"]
 __copyright__ = "Gramazio Kohler Research"
 __license__ = "MIT License"
 __email__ = ""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = []
